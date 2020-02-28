61703737f21692859ca77e962146dbe78e38b850
github username <ingrey1@gmail.com>
2020-02-28 15:42:40 -0800

    added api.js and helpers.js

---
+++ src/utilities/helpers.js
@@ -0,0 +1,18 @@
+export function constructItem(itemData){
+    // price, pic, title, sales, delivery_fee, shop_title
+    return {
+      external_id: itemData["num_iid"],
+      price: itemData["price"],
+      pic: itemData["pic"],
+      name: itemData["title"],
+      sales: itemData["sales"],
+      shop_name: itemData["shop_title"],
+      delivery_fee: itemData["delivery_fee"],
+      location: itemData["loc"]
+    }
+  }
+  
+  export function constructItems(itemData) {
+    debugger
+    return itemData.map(item => constructItem(item))
+  }
