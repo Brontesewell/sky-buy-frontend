a2a1c9b91cb6a20585fbc64fd9698a9a09726b0f
github username <github email address>
2020-02-28 15:43:50 -0800

    delete

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ItemCard from './ItemCard';
+import { Button } from 'react-bootstrap';
  
 class ShoppingCart extends React.Component {
   
@@ -9,13 +10,14 @@ class ShoppingCart extends React.Component {
     <div>
       <div className="row bot-army-row">
            <h1>Shopping Cart:</h1> 
-          {this.props.selectedItems.map(item => <ItemCard item={item} handleItemClick={this.props.removeSelectedItems}/>)}
+          {this.props.selectedItems.map(item => <div><ItemCard item={item}/> <Button className="ui button fluid" onClick={() => this.props.removeSelectedItems(this.props.item)}> Delete Item </Button> </div>)}
+          
           </div>
           </div>
     )
   }
 }
-
+// handleItemClick={this.props.removeSelectedItems}
 
  
 export default ShoppingCart;
